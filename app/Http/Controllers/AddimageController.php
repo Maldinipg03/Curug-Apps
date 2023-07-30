0797eb4dc1f47fcbc87a2590bee758e2cdf467b6
Maldini <19102280@ittelkom-pwt.ac.id>
2023-07-30 13:57:35 +0700

    curug-apss beta

---
+++ app/Http/Controllers/AddimageController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\addimage;
+use Illuminate\Http\Request;
+
+class AddimageController extends Controller
+{
+    public function index()
+    {
+        $mahasiswas = addimage::all();
+        return view('addimage.index', ['students' => $mahasiswas]);
+    }
+
+    public function create()
+    {
+        return view('addimage.create');
+    }
+
+
+    // fungsi untuk menambah gambar
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'nama' => 'required|min:3|max:50',
+            'image' => 'required|image|mimes:png,jpg',
+
+        ]);
+        $mahasiswa = new addimage();
+        $mahasiswa->name = $validateData['nama'];
+        if ($request->hasFile('image')) {
+            $extFile = $request->image->getClientOriginalExtension();
+            $namaFile = 'user-' . time() . "." . $extFile;
+            $path = $request->image->move('assets/images', $namaFile);
+            $mahasiswa->image = $path;
+        }
+        $mahasiswa->save();
+        $request->session()->flash('pesan', 'Penambahan data berhasil');
+        return redirect()->route('addimage.index');
+    }
+
+
+
+    // hapus gambar
+    public function delete($id)
+    {
+        $data = addimage::find($id);
+        $data->delete();
+        return redirect()->route('addimage.index')->with('success', 'Gambar Berhasil Di Hapus');
+    }
+
+
+    // fungsi untuk menampilkan data sebelum di edit
+    public function tampilfoto($id)
+    {
+        $data = addimage::find($id);
+        // dd($data);
+
+
+        return view('addimage.tampil', compact('data'));
+    }
+
+    // fungsi edit data gambar
+    public function update(Request $request, $id)
+    {
+
+        // $data = addimage::find($id);
+        // $data->update($request->except(['_token', 'nama', 'image']));
+        // // $data->update($request->except(['_token', 'nama', 'image', 'submit', '_method']));
+        // $data->update($request->all());
+        // if ($request->hasFile('image')) {
+        //     $extFile = $request->image->getClientOriginalExtension();
+        //     $namaFile = 'user-' . time() . "." . $extFile;
+        //     File::delete($id->image);
+        //     $path = $request->image->move('assets/images', $namaFile);
+        //     $data->image = $path;
+        // }
+
+        // return redirect()->route('addimage.index')->with('success', 'Data Berhasil Di Edit');
+        $validateData = $request->validate([
+            'nama' => 'required|min:3|max:50',
+            'image' => 'required|image|mimes:png,jpg',
+
+        ]);
+
+        $ubah = addimage::findorfail($id);
+        $awal = $ubah->image;
+
+        $data = [
+            'name' => $request['nama'],
+            'gambar' => $awal,
+        ];
+
+        $request->image->move('assets/images', $awal);
+        $ubah->update($data);
+        return redirect()->route('addimage.index');
+    }
+}
